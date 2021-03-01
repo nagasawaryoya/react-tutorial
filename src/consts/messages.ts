1647baa2e71725800c5332933d706d7d1b1ab095
nagasawaryoya <nagasawaryoya@gmail.com>
2021-03-02 07:41:24 +0900

    勝負が引き分けの際のメッセージ定義

--- src/consts/messages.ts
+++ src/consts/messages.ts
@@ -3,3 +3,4 @@ export const MSG_GO_TO_MOVE = 'Go to move #{0}';
 export const MSG_GO_TO_START = 'Go to game start';
 export const MSG_GO_TO_END = 'Go to game end';
 export const MSG_WINNER = 'Winner: {0}';
+export const MSG_DRAW = 'this game is draw';
