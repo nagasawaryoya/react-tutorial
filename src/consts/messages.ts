901323d43a1583ff8b257380aad0a739b77e8970
nagasawaryoya <nagasawaryoya@gmail.com>
2021-03-02 07:42:03 +0900

    typo修正

--- src/consts/messages.ts
+++ src/consts/messages.ts
@@ -3,4 +3,4 @@ export const MSG_GO_TO_MOVE = 'Go to move #{0}';
 export const MSG_GO_TO_START = 'Go to game start';
 export const MSG_GO_TO_END = 'Go to game end';
 export const MSG_WINNER = 'Winner: {0}';
-export const MSG_DRAW = 'this game is draw';
+export const MSG_DRAW = 'This game is draw';
